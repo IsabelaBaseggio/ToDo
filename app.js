2f522f777fb008929259ce8515b52ecf0de7e3c8
Isabela Ribeiro Baséggio <isabela.baseggio7@gmail.com>
2022-10-17 17:56:54 -0300

    Server Running

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+
+
+app.get('/', (req, res) => {
+    res.send('Iniciando')
+});
+
+
+app.listen(process.env.PORT, () => {
+    console.log('Server Running')
+})
