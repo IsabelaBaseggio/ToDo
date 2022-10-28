b261a94d9f9b8606ab0400698191ac2a33c76b36
Isabela Ribeiro Baséggio <isabela.baseggio7@gmail.com>
2022-10-28 17:43:57 -0300

    model Task taskSchema & controller TaskController createTask

--- controller/TaskController.js
+++ controller/TaskController.js
@@ -1,6 +1,22 @@
-const getAll = (req, res) => {
-    return res.render('index')
-}
+const Task = require("../models/Task");
 
+const getAllTasks = (req, res) => {
+  return res.render("index");
+};
 
-module.exports = { getAll }
+const createTask = async (req, res) => {
+  const task = req.body;
+
+  if (!task.task) {
+    return res.redirect("/");
+  }
+
+  try {
+    await Task.create(task);
+    return res.redirect("/");
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+};
+
+module.exports = { getAllTasks, createTask };
